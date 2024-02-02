71d5f09f66131925b52808b855cf41d0b5c6a5ec
Pablo Di Taranto <103513283+PDiTaranto@users.noreply.github.com>
2024-02-02 10:50:15 -0300

    Characters. ABPs

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Pablo Di Taranto (pablo.ditaranto@gmail.com)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
